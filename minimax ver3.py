d49d8ffe5e1df6b04eb591c741fdb7ad5c907c01
thrombe <yash.n.thombare620@gmail.com>
2021-03-16 11:59:47 +0530

    tried making the minimax work. but failed

--- minimax ver3.py
+++ minimax ver3.py
@@ -1,10 +1,6 @@
-
 '''
-currently ai dosent lose as player 1, but dosent win either.
-it draws every game.
-as player 2, it dosent do very good job.
-the first play is always is at c3, cuz loop ends at c3.
-
+this version has comPlay() a bit modified
+and code changed to make ai pl2
 
 
 '''
@@ -13,55 +9,43 @@ from terminal_tic_tac_toe import *
 
 
 def Mmax(board, score, pl1, pl2, ):
-    score = -1
-    for square in board:
-        if isValid(board, square, pl1, pl2) == 'valid':
-            #print(square)##
-            pl1.append(square)
-            if checkWin(pl1) == 'win':
-                score = 1
-                #print(pl1)##
-            else:
-                score = max(Mmin(board, score, pl1, pl2), score)
-            pl1.pop()
-    return score
-
-
-def Mmin(board, score, pl1, pl2 ):
-    score = 1
+    score[0] = -1
     for square in board:
         if isValid(board, square, pl1, pl2) == 'valid':
             #print(square)##
             pl2.append(square)
             if checkWin(pl2) == 'win':
-                score = -1
-                #print(score)##
-                #print(pl2)##
+                score[0] = 1
+                score[1] = square
+                #print(pl1)##
             else:
-                score = min(Mmax(board, score, pl1, pl2), score)
+                score[0] = max(Mmin(board, score, pl1, pl2)[0], score[0])
             pl2.pop()
     return score
 
-# plays 1 move and passes to minimax for deciding if its a good move
-def comPlay(board, pl1, pl2):
-    score = 0
+
+def Mmin(board, score, pl1, pl2 ):
+    score[0] = 1
     for square in board:
         if isValid(board, square, pl1, pl2) == 'valid':
             #print(square)##
             pl1.append(square)
             if checkWin(pl1) == 'win':
-            	pl1.pop()
-            	return square
-            score =  Mmin(board, score, pl1, pl2)
-            pl1.pop()
-            print(score)##
-            print(square)##
-            if score != -1:
-                bestNonWin = square
+                score[0] = -1
+                score[1] = square
                 #print(score)##
-                #print(square)##
-                pass
-    return bestNonWin
+                #print(pl2)##
+            else:
+                score[0] = min(Mmax(board, score, pl1, pl2)[0], score[0])
+            pl1.pop()
+    return score
+
+# plays 1 move and passes to minimax for deciding if its a good move
+def comPlay(board, pl1, pl2):
+    score = [0, '']
+    score =  Mmax(board, score, pl1, pl2)
+    #print(score)##
+    return score[1]
 
 
 
@@ -79,7 +63,7 @@ def startPvc():
 		else: # player 2
 			turn = '2O'
 			
-		# CHANGE 2 TO 1 AND 1 TO 2 HERE FOR PLAYER CHANGE
+		
 		if turn[0] == '1':
 			valid = 'no' # reset var for next turn
 			while valid == 'no': # check if play is valid
